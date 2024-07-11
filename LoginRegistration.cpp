94079411b013f23334baf7d12a72d337d4fb755d
Forrest Haugaard <forrestmountainskye@gmail.com>
2024-07-10 19:35:53 -0600

    Updating functions for user input functionality

--- LoginRegistration.cpp
+++ LoginRegistration.cpp
@@ -4,12 +4,32 @@
 
 using namespace std;
 
-void registration(string user, string pwd){
+void registration(){
+    string user, pwd;
+    string pwd1 = " ";
+    cout << "Please enter a username you will remember: ";
+    cin >> user;
+    
+
+    while(pwd != pwd1){
+        cout << "Please enter a password: ";
+        cin >> pwd;
+        
+
+        cout << "Please confirm your password: ";
+        cin >> pwd1;
+  
+
+        if(pwd != pwd1){
+            cout << "The password does not match! Please try again." << endl;
+        }
+    }
+
     fstream file;
     file.open("Accounts.csv", ios::app);
 
     file << user + "," + pwd << endl;
-    cout << "User has been registered!" << endl;
+    cout << user <<" has been registered!" << endl;
     file.close();
 }
 
@@ -33,8 +53,36 @@ bool login(string user, string pwd){
 }
 
 int main(){
-    string user = "Forrest";
-    string pwd = "Password";
-    login(user, pwd);
+    bool init = true;
+    int x;
+    string user, pwd;
+    while(init){
+        cout << "1. Login" << endl;
+        cout << "2. Register" << endl;
+        cout << "Please enter either 1 or 2: ";
+        cin >> x;
+        cout << endl;
+
+
+        if(x == 1){
+            cout << "Please enter your username: ";
+            cin >> user;
+            cout << "Please enter your password: ";
+            cin >> pwd;
+            if(login(user, pwd)){
+                init = false;
+            }
+            else{
+                cout << "User does not exist! Please register a new account!" << endl;
+                registration();
+                init = false;
+            }
+        }
+        else if(x == 2){
+            registration();
+            init = false;
+        }
+    }
+
     return 0;
 }
